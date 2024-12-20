15d9ab7178e2b426ce08a33b6d047ffc2c9bed3d
nicolelb137 <nicolelb137@gmail.com>
2024-12-19 21:59:57 -0500

    added api integration to perform search & update weather

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-function submitCitySearch(event) {
-  event.preventDefault();
-  let citySearchInput = document.querySelector("#user-entered-city");
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = citySearchInput.value;
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
-let searchFormElement = document.querySelector("#city-search");
+function searchCity(city) {
+  let apiKey = "be641daeo4c4132c8da3cfa3610475tb";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiURL).then(updateWeather);
+}
+
+function submitCitySearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#user-entered-city");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", submitCitySearch);
+
+searchCity("New York Cirty");
